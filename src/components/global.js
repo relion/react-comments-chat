37dbc832fb4379d8074bb9f5e88a94db3790bc8e
Aryeh Tuchfeld <aryeh.tuchfeld@gmail.com>
2020-12-03 02:17:15 +0200

    found the problem. need to change http server to port 80

--- src/components/global.js
+++ src/components/global.js
@@ -5,7 +5,7 @@ global.formatted_since_just_added = "just added";
 
 global.host = window.location.hostname; // "testcomments5-env.vip8dhubmp.us-west-2.elasticbeanstalk.com";
 //alert(host);
-var port = ":8080";
+var port = ":80";
 global.server_url = "http://" + global.host + port + "/handle_comments/"; // "http://www.thevcard.net/cms/cms_json.aspx";
 
 function handle_win_title() {
